31c85acf1572c6956864154eea64bea17977bf57
Bruno Krebs <bruno.krebs@auth0.com>
2018-11-02 15:29:36 -0300

    REST API

---
+++ src/main/java/com/auth0/microblog/MicroblogApplication.java
@@ -0,0 +1,18 @@
+package com.auth0.microblog;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+@ApplicationPath("/")
+@Path("/message")
+public class MicroblogApplication extends Application {
+    @GET
+    @Produces("text/plain")
+    public Response doGet() {
+        return Response.ok("Hello from Thorntail!").build();
+    }
+}
